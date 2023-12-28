5134d8694229ba609344a70d79d99d884413d107
navdeepmor <jaideepmor74@gmail.com>
2023-12-28 14:12:10 +0530

    carousel added to portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sk-sub-sec-con').slick({
+      // Slick Carousel settings/options
+      autoplay: false,
+      autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
+      // Add more settings as needed
+    });
+    $('.edu-sub-sec-con').slick({
+      // Slick Carousel settings/options
+      autoplay: false,
+      autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
+      // Add more settings as needed
+    });
+    $('.pro-sub-sec-con').slick({
+      // Slick Carousel settings/options
+      autoplay: false,
+      autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
+      // Add more settings as needed
+    });
+  });
